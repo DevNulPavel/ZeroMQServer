d8a91960b4347b2f690570a05b28352279b69451
DevNul <devnulpavel@gmail.com>
2016-10-22 11:42:12 +0300

    MultiServer

--- main.cpp
+++ main.cpp
@@ -3,15 +3,17 @@
 #include <string.h>
 #include <assert.h>
 #include <time.h>
-#include <random>
 #include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <functional>
 #include <zmq.h>
 
 
 
 //  Receive ZeroMQ string from socket and convert into C string
 //  Chops string at 255 chars, if it's longer
-static char* s_recv (void *socket) {
+char* s_recv (void *socket) {
     char buffer [256];
     int size = zmq_recv (socket, buffer, 255, 0);
     if (size == -1)
@@ -19,10 +21,10 @@ static char* s_recv (void *socket) {
     if (size > 255)
         size = 255;
     buffer [size] = 0;
-    return strdup (buffer);
+    return strdup(buffer);
 }
 
-static void s_send (void* socket, const char* message) {
+void s_send (void* socket, const char* message) {
     size_t length = strlen(message);    // TODO: нужно ли вычислять размер?
     zmq_send(socket, message, length, 0);
 }
@@ -40,7 +42,7 @@ int simpleServer(){
         char buffer [10];
         zmq_recv (responder, buffer, 10, 0);
         printf ("Received Hello\n");
-        sleep (1);          //  Do some 'work'
+        //sleep (1);          //  Do some 'work'
         zmq_send (responder, "World", 5, 0);
     }
 }
@@ -72,7 +74,54 @@ int pushServer(){
     return 0;
 }
 
+
+void* workerTask(void* context){
+    //  Сокет для работы с диспетчером
+    void* receiver = zmq_socket(context, ZMQ_REP);
+    zmq_connect (receiver, "inproc://workers");
+
+    while (1) {
+        char* string = s_recv(receiver);
+        printf ("Received request: [%s]\n", string);
+        free (string);
+        //  Выполняем задачу
+        sleep (1);
+        // отвечаем клиенту
+        s_send (receiver, "World");
+    }
+    zmq_close (receiver);
+    return nullptr;
+}
+
+int simpleMultThreaded(void) {
+
+    void* context = zmq_ctx_new();
+
+    //  Сокет для связи с клиентами
+    void* clients = zmq_socket (context, ZMQ_ROUTER);
+    zmq_bind (clients, "tcp://*:5555");
+
+    //  Сокет для связи с обработчиками
+    void* workers = zmq_socket (context, ZMQ_DEALER);
+    zmq_bind (workers, "inproc://workers");
+
+    //  Запускаем пул воркеров
+    for (int thread_nbr = 0; thread_nbr < 5; thread_nbr++) {
+        pthread_t worker;
+        pthread_create(&worker, NULL, workerTask, context);
+    }
+
+    //  Connect work threads to client threads via a queue proxy
+    zmq_proxy (clients, workers, NULL);
+
+    //  We never get here, but clean up anyhow
+    zmq_close (clients);
+    zmq_close (workers);
+    zmq_ctx_destroy (context);
+    return 0;
+}
+
 int main (void)
 {
-    return simpleServer();
+    return simpleMultThreaded();
 }
